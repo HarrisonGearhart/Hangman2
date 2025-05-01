e010cad2e6394e38f096a3ef17379d769fc9050b
Harrison  Gearhart <harrisongearhart@Harrisons-Air.attlocal.net>
2025-04-30 19:42:18 -0500

    make options blue after clicking/1

--- script.js
+++ script.js
@@ -128,7 +128,7 @@ const generateWord = (optionValue) => {
   let optionsButtons = document.querySelectorAll(".options");
   //If optionValur matches the button innerText then highlight the button
   optionsButtons.forEach((button) => {
-    if (button.innerText === optionValue) {
+    if (button.innerText.toLowerCase() === optionValue.toLowerCase()) {
       button.classList.add("active");
     }
     button.disabled = true;
